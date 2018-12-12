72e28abad91779127452a03230e447b6273cf6c9
Andy Peterson <apeterson@branch.io>
2018-12-12 14:42:29 -0800

    Added basic list adapter functionality

--- AdobeBranchDemo/src/androidTest/java/io/branch/sample/testadobebranch/BaseTest.java
+++ AdobeBranchDemo/src/androidTest/java/io/branch/sample/testadobebranch/BaseTest.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,12 +17,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+public class BaseTest {
+    private Context mContext;
 
-        assertEquals("io.branch.sample.testadobebranch", appContext.getPackageName());
+    @Before
+    public void setUp() {
+        mContext = InstrumentationRegistry.getTargetContext();
+    }
+
+    @After
+    public void tearDown() {
+        mContext = null;
+    }
+
+    @Test
+    public void testContext() {
+        assertEquals("io.branch.adobe.demo", mContext.getPackageName());
+    }
+
+    Context getTestContext() {
+        return mContext;
     }
 }

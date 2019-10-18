1c07abca591f0ee4d6b05bad1909ce33cc9161ed
bklastaitis-branch <benas.klastaitis@branch.io>
2019-10-17 19:19:43 -0700

    migrated the example project and the extension to androidx and upped adobe and branch sdk dependencies

--- AdobeBranchExample/src/main/java/io/branch/adobe/demo/ProductActivity.java
+++ AdobeBranchExample/src/main/java/io/branch/adobe/demo/ProductActivity.java
@@ -2,14 +2,16 @@ package io.branch.adobe.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -55,6 +57,7 @@ public class ProductActivity extends AppCompatActivity {
 
     @Override
     public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
         this.setIntent(intent);
     }
 

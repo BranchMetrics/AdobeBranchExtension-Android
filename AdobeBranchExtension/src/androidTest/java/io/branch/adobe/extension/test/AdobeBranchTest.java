1baa816794c15643fc3b7022635f0f0c29543069
Benas Klastaitis <benas.klastaitis@branch.io>
2020-09-11 15:02:59 -0700

    add tests

--- AdobeBranchExtension/src/androidTest/java/io/branch/adobe/extension/test/AdobeBranchTest.java
+++ AdobeBranchExtension/src/androidTest/java/io/branch/adobe/extension/test/AdobeBranchTest.java
@@ -1,47 +1,97 @@
 package io.branch.adobe.extension.test;
 
+import android.app.Application;
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.adobe.marketing.mobile.AdobeCallback;
+import com.adobe.marketing.mobile.Analytics;
+import com.adobe.marketing.mobile.Identity;
+import com.adobe.marketing.mobile.InvalidInitException;
+import com.adobe.marketing.mobile.Lifecycle;
+import com.adobe.marketing.mobile.LoggingMode;
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Signal;
+import com.adobe.marketing.mobile.UserProfile;
 
 import org.junit.Assert;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.lang.reflect.Method;
 
+import io.branch.adobe.extension.AdobeBranchExtension;
 import io.branch.referral.Branch;
+import io.branch.referral.PrefHelper;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 /**
  * Base Instrumented test, which will execute on an Android device.
  */
 @RunWith(AndroidJUnit4.class)
 public class AdobeBranchTest {
+    private static final String TAG = "AdobeBranchTest";
+    private static final String ADOBE_APP_ID = "d10f76259195/b0503e1a5dce/launch-9948a3b3a89d-development";
+    private Application app;
     private Context mContext;
+
+    @Rule
+    public ActivityTestRule<MockActivity> mActivityRule = new ActivityTestRule<>(MockActivity.class);
 
     @Before
     public void setUp() {
-        Branch.enableLogging();
-        mContext = InstrumentationRegistry.getContext();
+        app = mActivityRule.getActivity().getApplication();
+        mContext = getInstrumentation().getContext();
+
+        initAdobeBranch();
+    }
+
+    private void initAdobeBranch() {
+        PrefHelper.Debug("AdobeBranchTest.initAdobeBranch()");
+
+        MobileCore.setApplication(app);
+        MobileCore.setLogLevel(LoggingMode.DEBUG);
+
+        try {
+            Analytics.registerExtension();
+            UserProfile.registerExtension();
+            Identity.registerExtension();
+            Lifecycle.registerExtension();
+            Signal.registerExtension();
+            AdobeBranchExtension.registerExtension(app, true);
+            MobileCore.start(new AdobeCallback() {
+                @Override public void call(Object o) {
+                    MobileCore.configureWithAppID(ADOBE_APP_ID);
+                }
+            });
+        } catch (InvalidInitException e) {
+            PrefHelper.Debug("AdobeBranchTest.InvalidInitException: " + e.getLocalizedMessage());
+        }
     }
 
     @After
     public void tearDown() {
-        mContext = null;
         shutdownBranch();
+        app = null;
+        mContext = null;
     }
 
     @Test
     public void testAppContext() {
         // Context of the app under test.
-        Assert.assertNotNull(getTestContext());
+        Assert.assertNotNull(mContext);
     }
 
-    Context getTestContext() {
-        return mContext;
+    @Test
+    public void testBranchInstance() {
+        // Context of the app under test.
+        Assert.assertNotNull(Branch.getInstance());
     }
 
     private void shutdownBranch() {
@@ -53,6 +103,4 @@ public class AdobeBranchTest {
             Assert.fail();
         }
     }
-
-
 }
